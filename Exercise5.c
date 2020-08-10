a71fbb5cbfe2b4d852c3d1d0b2723be8c1caeab3
CSE-IU-Tutor1 <CSE-IU-Tutor1@users.noreply.github.com>
2020-08-11 00:28:46 +0700

    update

---
+++ Exercise5.c
@@ -0,0 +1,26 @@
+/*
+5. Consider a T9 phone keypad layout (i.e. numeric key 2 is also used to input a, b or c),
+1 and 0 are considered empty characters. Write a program to input a series of keypads pressed
+then output the possible combinations of letters.
+Ex:
+ ______________________________________
+| Input: 36                            |
+| Output: dm dn do em en eo fm fn fo   |
+|______________________________________|
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define SIZE 1000
+
+int ex5(char s[]){
+	//your codes here
+}
+
+int main(int argc, char *argv[]) {
+	char k[SIZE];
+	strcpy(k, argv[1]);
+    ex5(k);
+	return 0;
+}
