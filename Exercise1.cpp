0492bed55c954ddaedc014baf808516b924c29ef
CSE-IU-Tutor1 <CSE-IU-Tutor1@users.noreply.github.com>
2020-08-12 11:38:20 +0700

    update

---
+++ Exercise1.cpp
@@ -0,0 +1,26 @@
+/*
+1. Given an array of string, find the longest common prefix of all element.
+ ________________________________________________
+| Input: abc abd abf                             |
+| Output: ab                                     |
+|________________________________________________|
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+#include<limits.h>
+#define SIZE 1000
+
+void ex1(char *str[],int n){
+	//your codes here
+}
+
+int main(int argc, char *argv[]) {
+	
+	char *str[]={"abc","abcd","abe","abdr"};
+	ex1(str,4);
+			
+	return 0;
+}
---
+++ Exercise4.cpp
@@ -0,0 +1,27 @@
+/*
+4. Input 2 binary number using string then return the sum of those two.
+Result should use string.
+Ex:
+ __________________________________________________________
+| Input: 111  11                                           |
+| Output: 1010                                             |
+|__________________________________________________________|
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+#include<limits.h>
+#define SIZE 1000
+
+void ex4(char s1[], char s2[]){
+	//your codes here
+}
+
+int main(int argc, char *argv[]) {
+	char n1[] = "111";
+    char n2[] = "1111";
+    ex4(n1,n2);
+		
+	return 0;
+}
